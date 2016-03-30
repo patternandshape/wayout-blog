f7e9e55895d0b1d00baee38109acad1fea41338f
Molly Curtin and paul hess <patternandshape@gmail.com>
2016-03-30 11:31:39 -0700

    add working json file and connect firebase to project

---
+++ app/post/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    title: DS.attr(),
+    image: DS.attr(),
+    body: DS.attr(),
+    author: DS.attr(),
+    date: DS.attr(),
+    category: DS.attr()
+});
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'wayout-blog',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://way-out.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     googleFonts: [
