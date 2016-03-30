965500c0b6f80b116bf0db8d85e5f068abdc6b9e
Molly Curtin and paul hess <Guest@epicodus-12.local>
2016-03-30 14:53:53 -0700

    add sidebar hack

---
+++ app/index/route.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      posts: this.store.findAll('post'),
+      authors: this.store.findAll('author'),
+      categories: this.store.findAll('category')
+    });
+  }
+});
