7a242e17ab5ca98f5f377ed089f5c7b4faa287d9
Molly Curtin and Paul Hess <pauljacksonhess@gmail.com>
2016-03-31 11:50:57 -0700

    add delete post function to post pages

--- app/post/route.js
+++ app/post/route.js
@@ -4,7 +4,15 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
   },
-  model2() {
-    return this.store.findAll('post');
+  // model2() {
+  //   return this.store.findAll('post');
+  // }
+  actions: {
+    delete(model){
+      if(confirm('Are you sure you want to delete this post?')){
+        model.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
