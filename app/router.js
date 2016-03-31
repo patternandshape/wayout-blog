85b3008166ea38d08236999d313c1a6baae96bd8
Molly Curtin and Paul Hess <pauljacksonhess@gmail.com>
2016-03-31 10:48:41 -0700

    create about and contact pages and make them accessible in the sidebar on all pages

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ Router.map(function() {
   this.route('post', {path: '/post/:post_id'});
   this.route('author', {path: '/author/:author_id'});
   this.route('category', {path: '/category/:category_id'});
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
